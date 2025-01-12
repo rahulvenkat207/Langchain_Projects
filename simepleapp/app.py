f0145883433717e8aa0871f2a60dda1e41a4f988
Rahul <159618613+rahulvenkat207@users.noreply.github.com>
2025-01-12 06:32:42 +0530

    Add files via upload

---
+++ simepleapp/app.py
@@ -0,0 +1,40 @@
+import os
+from dotenv import load_dotenv
+
+from langchain_community.llms import Ollama
+import streamlit as st
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+
+load_dotenv()
+
+os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
+os.environ["LANGCHAIN_TRACING_V2"] ="true"
+os.environ["LANGCHAIN_PROJECT"] = os.getenv("LANGCHAIN_PROJECT")
+
+## Propmt Template
+
+prompt = ChatPromptTemplate(
+
+    [
+        ("system","You are a helpful assistant.Please respond to the question asked"),
+        ("user","Question:{question}")
+    ]
+)
+
+## Streamlit Framework
+st.title("Langchain Demo  with Gemma model")
+
+input_text = st.text_input("What questions you have in mind?")
+
+## OLLAMA Gemini model
+
+llm = Ollama(model="gemma:2b")
+
+output_parser = StrOutputParser()
+
+chain = prompt|llm|output_parser
+
+if input_text:
+    st.write(chain.invoke({"question": input_text}))
